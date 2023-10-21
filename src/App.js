1e5071942b077d02e728398d864f9998d5be6956
diegoprado86 <diego_prado1986@outlook.com>
2023-10-21 12:20:27 -0300

    instalação do styled components, criação da Home e da Styledinstalação do styled components, criação da Home e da Styled

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './header/Header';
+import Home from './home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Catálogo de Filmes
-        </h1>
-      </header>
+    <div>
+        <Header/>
+        <Home/>
     </div>
   );
 }
---
+++ src/home/Home.js
@@ -0,0 +1,27 @@
+import { Container } from "./Style";
+
+const  Home = () => {
+    return (
+        <Container>
+            <h2>
+                Catálogo
+            </h2>
+            <ul>
+                <li>
+                    <img src = "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg" alt ="Supernatural" />
+                    <span>Supernatural</span>
+                </li>
+                <li>
+                <img src = "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg" alt ="Supernatural" />
+                    <span>Supernatural</span>
+                </li>
+                <li>
+                <img src = "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg" alt ="Supernatural" />
+                    <span>Supernatural</span>
+                </li>
+            </ul>
+        </Container>
+    )
+}
+
+export default Home;
