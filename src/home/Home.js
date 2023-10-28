7662a7f8b0b33854ac627800db65f59d9c7b7fc8
diegoprado86 <diego_prado1986@outlook.com>
2023-10-28 12:14:10 -0300

    inclusão de useState, e colocar o styled nas tags

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,25 +1,56 @@
 import { Container } from "./Style";
+import { MovieList } from "./Style";
+import { Movie } from "./Style";
+import { APIKEY } from "../config/key";
+import { useState, useEffect } from "react";
 
-const  Home = () => {
+const Home = () => {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+            fetch(`https://api.themoviedb.org/3/movie/popular?api_key=2ca898872acc8214d15saa40e33600aa0&language=en-US&page=1`)
+    },[])
+    
+
+    // const movies = [
+    //     {
+    //         title: "Supernatural",
+    //         image_url: "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg"
+    //     },
+
+    //     {
+    //         title: "Supernatural 2",
+    //         image_url: "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg"
+    //     },
+
+    //     {
+    //         title: "Supernatural 3",
+    //         image_url: "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg"
+    //     }
+    // ]
+
+
     return (
         <Container>
             <h2>
                 Catálogo
             </h2>
-            <ul>
-                <li>
-                    <img src = "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg" alt ="Supernatural" />
-                    <span>Supernatural</span>
-                </li>
-                <li>
-                <img src = "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg" alt ="Supernatural" />
-                    <span>Supernatural</span>
-                </li>
-                <li>
-                <img src = "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg" alt ="Supernatural" />
-                    <span>Supernatural</span>
-                </li>
-            </ul>
+            <MovieList>
+
+                {
+                    movies.map(movie => {
+                        return (
+                            <Movie>
+                                <a href="https://www.google.com">
+                                    <img src={movie.image_url} alt={movie.title} />
+                                </a>
+                                <span>{movie.title}</span>
+                            </Movie>
+                        )
+                    })
+                }
+            </MovieList>
         </Container>
     )
 }
