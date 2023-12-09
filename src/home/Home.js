d528cbface01ae0b7542d235cfdffe2cfb6478d6
DiegoPrado86 <111581331+DiegoPrado86@users.noreply.github.com>
2023-12-09 10:21:36 -0300

    Commit

---
+++ src/home/Home.js
@@ -0,0 +1,64 @@
+import { Container } from "./Style";
+import { MovieList } from "./Style";
+import { Movie } from "./Style";
+import { APIKEY } from "../config/key";
+import { useState, useEffect } from "react";
+
+const Home = () => {
+    const img_path = 'https://image.tmdb.org/t/p/w500/';
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+            fetch(`
+            https://api.themoviedb.org/3/tv/popular?api_key=${APIKEY}&language=pt-BR&page=1`)
+            .then(response => response.json())
+            .then(data => {
+                setMovies(data.results)
+            })
+    },[])
+    
+
+    // const movies = [
+    //     {
+    //         title: "Supernatural",
+    //         image_url: "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg"
+    //     },
+
+    //     {
+    //         title: "Supernatural 2",
+    //         image_url: "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg"
+    //     },
+
+    //     {
+    //         title: "Supernatural 3",
+    //         image_url: "https://img.elo7.com.br/product/zoom/1D05C86/poster-supernatural-tamanho-90x-0-cm-lo008-geek.jpg"
+    //     }
+    // ]
+
+
+    return (
+        <Container>
+            <h2>
+                Catálogo
+            </h2>
+            <MovieList>
+
+                {
+                    movies.map(movie => {
+                        return (
+                            <Movie>
+                                <a href="#">
+                                    <img src={`${img_path}${movie.poster_path}`} alt={movie.title} />
+                                </a>
+                                <span>{movie.title}</span>
+                            </Movie>
+                        )
+                    })
+                }
+            </MovieList>
+        </Container>
+    )
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import './global.css';
+import App from './App';
+import Details from './details/Details';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App/>}/>
+          <Route path='/details' element={<Details/>}/>
+        </Routes>
+    </BrowserRouter>
+);
+
+
